5ad681527833beabce6f25c4010b5484fc6d0498
CarlosArroniz <fredyarroniz@hotmail.com>
2016-06-22 16:16:57 -0500

    working with Script Generator

--- ScioAutomationFramework/Extenciones/ScriptsGenerator.cs
+++ ScioAutomationFramework/Extenciones/ScriptsGenerator.cs
@@ -8,6 +8,9 @@ namespace ScioAutomationFramework.Extenciones
 {
     #region
 
+    using System;
+    using System.IO;
+    using System.Reflection;
     using System.Windows.Forms;
     using System.Xml.Linq;
 
@@ -19,15 +22,25 @@ namespace ScioAutomationFramework.Extenciones
         /// <summary>The script gen.</summary>
         public static void ScriptGen()
         {
-            var xelement =
-                XElement.Load(
-                    "C:\\Users\\caarroniz\\Desktop\\AutomationFramework\\ScioAutomationFramework\\Data\\Script.xml");
+
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var path = dir.Split(new[] { "bin" }, StringSplitOptions.None)[0];
+
+            var xelement = XElement.Load(path + "\\Data\\Script.xml");
 
             var elementsActions = xelement.Elements();
 
             foreach (var el in elementsActions)
             {
                 MessageBox.Show(el.Element("ElId").Value);
+
+                string element, action, value;
+
+                element = el.Element("ElId").Value;
+                action = el.Element("Action").Value;
+                value = el.Element("Value").Value;
+                
             }
         }
     }
